130e5b313ff6e4fd9200caaf0cd6dbf881f1e5b0
dlwlsdlf <dlwlsdlf0723@naver.com>
2021-02-20 19:53:13 +0900

    user.vo 나름대로 작성해보기

--- src/main/java/kr/or/comma/user/vo/UserVO.java
+++ src/main/java/kr/or/comma/user/vo/UserVO.java
@@ -1,5 +1,77 @@
 package kr.or.comma.user.vo;
 
+import java.sql.Date;
+
 public class UserVO {
 
+	private int user_no;
+	private String user_id;
+	private String user_email;
+	private String user_name;
+	private String user_password;
+	private String user_intro;
+	private String user_profile;
+	private Date user_creat_dt;
+	private String user_authority;
+	private boolean  user_enabled;
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_password() {
+		return user_password;
+	}
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	public String getUser_intro() {
+		return user_intro;
+	}
+	public void setUser_intro(String user_intro) {
+		this.user_intro = user_intro;
+	}
+	public String getUser_profile() {
+		return user_profile;
+	}
+	public void setUser_profile(String user_profile) {
+		this.user_profile = user_profile;
+	}
+	public Date getUser_creat_dt() {
+		return user_creat_dt;
+	}
+	public void setUser_creat_dt(Date user_creat_dt) {
+		this.user_creat_dt = user_creat_dt;
+	}
+	public String getUser_authority() {
+		return user_authority;
+	}
+	public void setUser_authority(String user_authority) {
+		this.user_authority = user_authority;
+	}
+	public boolean isUser_enabled() {
+		return user_enabled;
+	}
+	public void setUser_enabled(boolean user_enabled) {
+		this.user_enabled = user_enabled;
+	}
 }
