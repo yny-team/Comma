d28f3cc6191547fa248ce4e37e8a4c2c0cfd2904
jaedeokhan <dgd03023@cu.ac.kr>
2021-02-22 14:58:54 +0900

    add user

--- src/main/java/kr/or/comma/user/controller/UserController.java
+++ src/main/java/kr/or/comma/user/controller/UserController.java
@@ -1,16 +1,5 @@
 package kr.or.comma.user.controller;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class UserController {
-
-	@RequestMapping("/home.jsp")
-	public String main() {
-		return "home";
-=======
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -28,7 +17,6 @@ public class UserController {
 		logger.info("signin..............");
 		
 		return "user/login";
->>>>>>> 064e24c735026085758ffaadba3797e18d0e6c34
 	}
 	
 }
